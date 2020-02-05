3bfc01603cf3fad8743123de721a1b1c9ba5c23b
muna <muna.jebril1995@hotmail.com>
2020-02-05 16:49:43 +0200

    this is realy lab6

---
+++ java.js
@@ -0,0 +1,14 @@
+
+   var name=  prompt("hello enter your name");
+     alert("hello"+name);
+  
+    var chick= confirm("would you like to conitunies");
+    if (chick ==true)
+    {  var age=  prompt("what your age ");
+    
+confirm("good bye ")
+     
+    }
+
+ 
+ 
