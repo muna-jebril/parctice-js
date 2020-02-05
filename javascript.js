09ea2d40a8177c8a0c2a5f36cc25e93ed9b66eb4
muna <muna.jebril1995@hotmail.com>
2020-02-05 09:28:43 +0200

    this is js modfied

---
+++ javascript.js
@@ -0,0 +1,18 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+if ( hourNow>18)
+{
+    greeting='Good evening';
+
+}else if (hourNow>12){
+    greeting='good afternoun';
+
+}
+else if (hourNow>0)
+{
+    greeting='good morning';
+}else{
+    greeting='welcome'
+}
+document.write('<h3>'+greeting+'</h3>');
